0a8917c49e749aa8ad439736490ca0a75f80ecf2
sidhiverdhan <sidhi.verdhan@gmail.com>
2024-03-28 14:38:18 -0400

    updated code

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-file1 ='/Users/sidhiverdhan/Documents/samle_pdfs/order1.pdf'
-file2 = '/Users/sidhiverdhan/Documents/samle_pdfs/wrdsb.pdf'
+file1 ='order1.pdf'
+file2 = 'order2.pdf'
 mergedFile ='MergedPdf.pdf'
